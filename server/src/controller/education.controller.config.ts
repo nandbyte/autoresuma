dd8ac981f48716615853485b75f0e5d55f83f414
zedrex <adib14021999@gmail.com>
2021-10-07 23:40:33 +0600

    Minor fix

--- server/src/controller/education.controller.config.ts
+++ server/src/controller/education.controller.config.ts
@@ -1,148 +1,148 @@
-import { CommonControllerConfig } from '../common/common.controllers.config'
-import Education from '../models/education'
-import { v4 as uuidv4 } from 'uuid'
-import { Request, Response } from 'express'
+import { CommonControllerConfig } from "../common/common.controllers.config";
+import Education from "../models/education";
+import { v4 as uuidv4 } from "uuid";
+import { Request, Response } from "express";
 
 class EducationController extends CommonControllerConfig {
     create = async (req: Request, res: Response) => {
         try {
-            const id = uuidv4()
-            const record = await Education.create({ ...req.body, id })
+            const id = uuidv4();
+            const record = await Education.create({ ...req.body, id });
             return res.json({
-                status: 200,
-                msg: 'OK',
-                route: '/v1/education',
+                status: 201,
+                msg: "OK",
+                route: "/v1/education",
                 record,
-            })
+            });
         } catch (error: any) {
             return res.json({
                 status: 500,
                 msg: error.message,
-                route: '/v1/education',
-            })
+                route: "/v1/education",
+            });
         }
-    }
+    };
 
     getAll = async (req: Request, res: Response) => {
         try {
-            const uid = req.params.userId
-            const record = await Education.findAll({ where: { userId: uid } })
+            const uid = req.params.userId;
+            const record = await Education.findAll({ where: { userId: uid } });
 
             return res.json({
                 status: 200,
-                msg: 'OK',
-                route: '/v1/education',
+                msg: "OK",
+                route: "/v1/education",
                 record,
-            })
+            });
         } catch (error: any) {
             return res.json({
                 status: 500,
                 msg: error.message,
-                router: '/v1/education',
-            })
+                router: "/v1/education",
+            });
         }
-    }
+    };
 
     getById = async (req: Request, res: Response) => {
         try {
-            const uid = req.params.userId
-            const eduId = req.params.educationId
+            const uid = req.params.userId;
+            const eduId = req.params.educationId;
             const record = await Education.findOne({
                 where: {
                     id: eduId,
                     userId: uid,
                 },
-            })
+            });
 
             if (!record) {
                 return res.json({
                     status: 204,
-                    msg: 'NO CONTENT',
-                    route: '/v1/education/:userId/:educationId',
-                })
+                    msg: "NO CONTENT",
+                    route: "/v1/education/:userId/:educationId",
+                });
             }
 
             return res.json({
                 status: 200,
-                msg: 'OK',
-                route: '/v1/education/:userId/:educationId',
-            })
+                msg: "OK",
+                route: "/v1/education/:userId/:educationId",
+            });
         } catch (error: any) {
             return res.json({
                 status: 500,
                 msg: error.message,
-                route: '/v1/education/:userId/:educationId',
-            })
+                route: "/v1/education/:userId/:educationId",
+            });
         }
-    }
+    };
 
     update = async (req: Request, res: Response) => {
         try {
-            const uid = req.params.userId
-            const eduId = req.params.educationId
+            const uid = req.params.userId;
+            const eduId = req.params.educationId;
             const record = await Education.findOne({
                 where: {
                     id: eduId,
                     userId: uid,
                 },
-            })
+            });
 
             if (!record) {
                 return res.json({
                     status: 204,
-                    msg: 'NO CONTENT',
-                    route: '/v1/education/:userId/:educationId',
-                })
+                    msg: "NO CONTENT",
+                    route: "/v1/education/:userId/:educationId",
+                });
             }
 
-            const newRec = await record.update({ ...req.body })
+            const newRec = await record.update({ ...req.body });
             return res.json({
                 status: 200,
-                msg: 'OK',
-                route: '/v1/education/:userId/:educationId',
+                msg: "OK",
+                route: "/v1/education/:userId/:educationId",
                 newRec,
-            })
+            });
         } catch (error: any) {
             return res.json({
                 status: 500,
                 msg: error.message,
-                route: '/v1/education/:userId/:educationId',
-            })
+                route: "/v1/education/:userId/:educationId",
+            });
         }
-    }
+    };
 
     delete = async (req: Request, res: Response) => {
         try {
-            const uid = req.params.userId
-            const eduId = req.params.educationId
+            const uid = req.params.userId;
+            const eduId = req.params.educationId;
             const record = await Education.findOne({
                 where: {
                     id: eduId,
                     userId: uid,
                 },
-            })
+            });
 
             if (!record) {
                 return res.json({
                     status: 204,
-                    msg: 'NO CONTENT',
-                    route: '/v1/education/:userId/:educationId',
-                })
+                    msg: "NO CONTENT",
+                    route: "/v1/education/:userId/:educationId",
+                });
             }
-            await record.destroy()
+            await record.destroy();
             return res.json({
                 status: 200,
-                msg: 'OK',
-                route: '/v1/education/:userId/:educationId',
-            })
+                msg: "OK",
+                route: "/v1/education/:userId/:educationId",
+            });
         } catch (error: any) {
             return res.json({
                 status: 500,
                 msg: error.message,
-                route: '/v1/education/:userId/:educationId',
-            })
+                route: "/v1/education/:userId/:educationId",
+            });
         }
-    }
+    };
 }
 
-export default new EducationController()
+export default new EducationController();
--- server/src/controller/skill.controller.config.ts
+++ server/src/controller/skill.controller.config.ts
@@ -1,101 +1,150 @@
-
-
 import { CommonControllerConfig } from "../common/common.controllers.config";
 import Skill from "../models/skill";
-import {v4 as uuidv4} from "uuid";
-import { Request,Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+import { Request, Response } from "express";
 
+class SkillController extends CommonControllerConfig {
+    create = async (req: Request, res: Response) => {
+        const id = uuidv4();
+        try {
+            const record = await Skill.create({ ...req.body, id });
+            return res.json({
+                status: 201,
+                msg: "OK",
+                route: "/v1/skill",
+                record,
+            });
+        } catch (e) {
+            return res.json({
+                status: 500,
+                msg: e.message,
+                route: "/v1/skill",
+            });
+        }
+    };
 
-class SkillController extends CommonControllerConfig{
+    getAll = async (req: Request, res: Response) => {
+        try {
+            const uid = req.params.userId;
+            const records = await Skill.findAll({
+                where: {
+                    userId: uid,
+                },
+            });
+            return res.json({
+                status: 200,
+                msg: "OK",
+                route: "/v1/skill/:userId",
+                records,
+            });
+        } catch (e) {
+            return res.json({
+                status: 500,
+                msg: e.message,
+                route: "/v1/skill/:userId",
+            });
+        }
+    };
 
-   	create = async(req: Request,res: Response)=>{
-		const id = uuidv4();
-		try{
-			const record = await Skill.create({...req.body,id});
-			return res.json({status:201,msg:"OK",route:"/v1/skill",record});
-		}
-		catch(e){
-			return res.json({status:500,msg:e.message,route:"/v1/skill"});
-		}
-	}
+    getById = async (req: Request, res: Response) => {
+        try {
+            const uid = req.params.userId;
+            const skillId = req.params.skillId;
+            const record = await Skill.findOne({
+                where: {
+                    id: skillId,
+                    userId: uid,
+                },
+            });
 
+            if (!record) {
+                return res.json({
+                    status: 204,
+                    msg: "NO CONTENT",
+                    route: "/v1/skill/:userId/:skillId",
+                });
+            }
 
-	 getAll = async(req: Request,res: Response)=>{
-		try{
-			const uid = req.params.userId;
-			const records = await Skill.findAll({where: {
-				userId: uid
-			}});
-			return res.json({status:200,msg:"OK",route:"/v1/skill/:userId",records});
-		}catch(e){
-			return res.json({status:500,msg:e.message,route:"/v1/skill/:userId"});
-		}
-	}
+            return res.json({
+                status: 200,
+                msg: "OK",
+                route: "/v1/skill/:userId/:skillId",
+            });
+        } catch (e) {
+            return res.json({
+                status: 500,
+                msg: e.message,
+                route: "/v1/skill/:userId/:skillId",
+            });
+        }
+    };
 
+    update = async (req: Request, res: Response) => {
+        try {
+            const uid = req.params.userId;
+            const skillId = req.params.skillId;
+            const record = await Skill.findOne({
+                where: {
+                    id: skillId,
+                    userId: uid,
+                },
+            });
 
-	 getById = async(req: Request,res: Response)=>{
-		try{
+            if (!record) {
+                return res.json({
+                    status: 204,
+                    msg: "NO CONTENT",
+                    route: "/v1/skill/:userId/:skillId",
+                });
+            }
+            const newRec = await record.update({ ...req.body });
+            return res.json({
+                status: 200,
+                msg: "OK",
+                route: "/v1/skill/:userId/:skillId",
+                newRec,
+            });
+        } catch (e) {
+            return res.json({
+                status: 500,
+                msg: e.message,
+                route: "/v1/skill/:userId/:skillId",
+            });
+        }
+    };
 
-			const uid = req.params.userId;
-			const skillId = req.params.skillId;
-			const record = await Skill.findOne({where:{
-				id : skillId,
-				userId: uid
-			}});
+    delete = async (req: Request, res: Response) => {
+        try {
+            const uid = req.params.userId;
+            const skillId = req.params.skillId;
 
-			if(!record){
-				return res.json({status:204,msg:"NO CONTENT",route:"/v1/skill/:userId/:skillId"});
-			}
-
-			return res.json({status:200,msg:"OK",route:"/v1/skill/:userId/:skillId"});
-		}
-		catch(e){
-			return res.json({status:500,msg:e.message,route:"/v1/skill/:userId/:skillId"});
-		}
-	}
-
-	update = async(req: Request, res: Response)=>{
-		try{
-			const uid = req.params.userId;
-			const skillId = req.params.skillId;
-			const record = await Skill.findOne({where:{
-				id: skillId,
-				userId: uid
-			}});
-
-			if(!record){
-				return res.json({status:204,msg:"NO CONTENT",route:"/v1/skill/:userId/:skillId"});
-			}
-			const newRec = await record.update({...req.body});
-			return res.json({status:200,msg:"OK",route:"/v1/skill/:userId/:skillId",newRec});
-
-		}catch(e){
-			return res.json({status:500,msg:e.message,route:"/v1/skill/:userId/:skillId"});
-		}
-	}
-
-	delete = async(req: Request, res: Response)=>{
-		try{
-			const uid = req.params.userId;
-			const skillId = req.params.skillId;
-
-			const record = await Skill.findOne({where:{
-				id: skillId,
-				userId:uid
-			}});
-			if(!record){
-				return res.json({status:204,msg:"NO CONTENT",route:"/v1/skill/:userId/:skillId"});
-			}
-			await record.destroy();
-			return res.json({status:200,msg:"OK",route:"/v1/skill/:userId/:skillId"});
-		}
-		catch(e){
-			return res.json({status:500,msg:e.message,route:"/v1/skill/:userId/:skillId"});
-		}
-	}
-
+            const record = await Skill.findOne({
+                where: {
+                    id: skillId,
+                    userId: uid,
+                },
+            });
+            if (!record) {
+                return res.json({
+                    status: 204,
+                    msg: "NO CONTENT",
+                    route: "/v1/skill/:userId/:skillId",
+                });
+            }
+            await record.destroy();
+            return res.json({
+                status: 200,
+                msg: "OK",
+                route: "/v1/skill/:userId/:skillId",
+            });
+        } catch (e) {
+            return res.json({
+                status: 500,
+                msg: e.message,
+                route: "/v1/skill/:userId/:skillId",
+            });
+        }
+    };
 }
-
-
 
 export default new SkillController();
