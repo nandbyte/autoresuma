6623bd31b7089042c26a4feb44e299a242edd936
zedrex <adib14021999@gmail.com>
2021-10-09 12:55:11 +0600

    Added showcase ui support

--- client/src/pages/BioTab/BioTab.tsx
+++ client/src/pages/BioTab/BioTab.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box } from "@chakra-ui/layout";
+import { Box, Stack, Heading } from "@chakra-ui/layout";
+import { FormControl, FormLabel } from "@chakra-ui/react";
 
 import BioForm from "../../components/BioForm";
 import BioView from "../../components/BioView";
@@ -9,23 +10,42 @@ import { Redirect } from "react-router";
 const BioTab = () => {
     const { updating } = useTypedSelector((state) => state.bio);
 
-    const { loggedIn } = useTypedSelector((state) => state.user);
+    const { loggedIn, user } = useTypedSelector((state) => state.user);
     if (!loggedIn) {
-        return <Redirect to="/" />
+        return <Redirect to="/" />;
     }
-    
+
     return (
-        <Box
-            w="100%"
-            bgColor="gray.700"
-            color="white"
-            p={6}
-            borderRadius="md"
-            my={6}
-        >
-            {!updating ? <BioView /> : null}
-            {updating ? <BioForm /> : null}
-        </Box>
+        <Stack spacing="12">
+            <Box
+                w="100%"
+                bgColor="gray.700"
+                color="white"
+                p={6}
+                borderRadius="md"
+                my={6}
+            >
+                {!updating ? <BioView /> : null}
+                {updating ? <BioForm /> : null}
+            </Box>
+            <Box
+                w="100%"
+                bgColor="gray.700"
+                color="white"
+                p={6}
+                borderRadius="md"
+                my={6}
+            >
+                <FormControl id="showcane-name">
+                    <FormLabel>
+                        <Heading variant="label">Showcase Link</Heading>
+                    </FormLabel>
+                    <FormLabel>
+                        {"http://localhost:3001/showcase/" + user?.id}
+                    </FormLabel>
+                </FormControl>
+            </Box>
+        </Stack>
     );
 };
 
